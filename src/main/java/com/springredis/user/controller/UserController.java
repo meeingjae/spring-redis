9a2a8962fcdbd78b7f2229bacb629a32e83825fb
Minjae Kim <minjae.kim@mytrum.com>
2022-06-10 15:08:03 +0900

    feat: User 전부 삭제 (Reset) API 구현 및 Logger 추가
    Logger 설정 추가

--- src/main/java/com/springredis/user/controller/UserController.java
+++ src/main/java/com/springredis/user/controller/UserController.java
@@ -5,8 +5,11 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.IntStream;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StopWatch;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +22,8 @@ import com.springredis.user.repository.UserRepository;
 @RestController
 @RequestMapping(value = "/user")
 public class UserController {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     private final UserRepository userRepository;
     private final RedisUserRepository redisUserRepository;
@@ -41,9 +46,14 @@ public class UserController {
         stopWatch.start();
         userRepository.saveAll(users);
         stopWatch.stop();
-        System.out.println(stopWatch.prettyPrint());
+        logger.info("save takes {} seconds", stopWatch.getTotalTimeSeconds());
         return ResponseEntity.ok(count);
     }
 
+    @DeleteMapping(value = "/all")
+    public void deleteAllUsers() {
+        redisUserRepository.deleteAll();
+        userRepository.deleteAll();
+    }
 
 }
